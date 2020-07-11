5a34162e2b99a02db522722b5ae5145e63961d28
Tamires Cordeiro <tamirescordeiro@Mac-mini-de-Tamires.local>
2020-07-11 17:22:21 -0300

    recursão funcionando sem teste

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-function readFile(pathCLI, callback){
-  fs.readFile(pathCLI, 'utf-8', (error, content) => {
-    if(error){
-      throw console.error('could not read the document');
-    } 
-    content;
-    callback();
-  });
-};
 
 module.exports = function mdLinks(pathCLI){
   if(path.extname(pathCLI) !== '.md'){
     console.error('not a markdown file');
   } 
-  readFile(pathCLI, (markdownText) => console.log(markdownText));
+  readFile(pathCLI);
+};
+
+function readFile(pathCLI){
+  fs.readFile(pathCLI, 'utf-8', showContent);
+};
+
+function showContent(error, content){
+  if (error) return console.error('could not show content');
+  console.log(content);
 };
